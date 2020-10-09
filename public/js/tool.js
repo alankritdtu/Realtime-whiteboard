92ef0f78c3ee851c8dbe8d95f78dc6bdcea67571
Alankrit <alankrit.agr99@gmail.com>
2020-10-09 13:04:49 +0530

    3rd

--- public/js/tool.js
+++ public/js/tool.js
@@ -1,5 +1,5 @@
 // Connect to socket server
-const socket = io.connect("http://wbboard.herokuapp.com/") ;
+const socket = io.connect("https://project-white-board.herokuapp.com/") ;
 
 const board = document.querySelector(".board") ;
 board.height = window.innerHeight ;
